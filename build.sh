42cc52282343124cd86abdf61f339c8022ce1949
Callum Jones <callum@callumj.com>
2014-07-16 19:30:04 +0800

    Correcting build script

--- build.sh
+++ build.sh
@@ -20,14 +20,53 @@ then
   fi
 fi
 
+rm -r -f tmp/go
+rm -r -f builds/
+
 # vet the source (capture errors because the current version does not use exit statuses currently)
+echo "Vetting..."
 VET=`go tool vet . 2>&1 >/dev/null`
+
+cur=`pwd` 
 
 if ! [ -n "$VET" ]
 then
   echo "All good"
-  goxc -os "linux windows darwin" -pv ${VERSION} -d builds xc copy-resources archive-zip archive-tar-gz pkg-build rmbin
+  mkdir -p tmp/go
+  mkdir builds
+  mkdir tmp/go/src tmp/go/bin tmp/go/pkg
+  mkdir -p tmp/go/src/github.com/callumj/busan
+  cp -R app remote utils main.go tmp/go/src/github.com/callumj/busan/
+
+  go_src=$'package utils\nconst BusanVersion string = "'"${VERSION}"$'"'
+  echo "$go_src" > tmp/go/src/github.com/callumj/busan/utils/version.go
+
+  mkdir -p builds/${VERSION}/darwin_386 builds/${VERSION}/darwin_amd64 builds/${VERSION}/linux_386 builds/${VERSION}/linux_amd64 
+  GOPATH="${cur}/tmp/go"
+  echo "Getting"
+  GOPATH="${cur}/tmp/go" go get -d .
+  echo "Starting build"
+
+  GOPATH="${cur}/tmp/go" GOOS=darwin GOARCH=386 go build -o builds/${VERSION}/darwin_386/busan
+
+  GOPATH="${cur}/tmp/go" GOARCH=amd64 GOOS=darwin go build -o builds/${VERSION}/darwin_amd64/busan
+
+  GOPATH="${cur}/tmp/go" GOOS=linux GOARCH=amd64 go build -o builds/${VERSION}/linux_amd64/busan
+
+  GOPATH="${cur}/tmp/go" GOOS=linux GOARCH=386 go build -o builds/${VERSION}/linux_386/busan
 else
   echo "$VET"
   exit -1
 fi
+
+# rewrite the binaries
+
+FILES=builds/${VERSION}/*/busan
+for f in $FILES
+do
+  str="/busan"
+  repl=""
+  path=${f/$str/$repl}
+  tar  -C ${path} -cvzf "${f}.tgz" busan
+  rm ${f}
+done
